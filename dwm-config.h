1a998e2727e6d149ada890e4d939acbbc6b84143
Anton Lilja <12533691+antlilja@users.noreply.github.com>
2021-06-02 13:56:30 +0200

    Update dwm-config.h

--- dwm-config.h
+++ dwm-config.h
@@ -72,7 +72,7 @@ shiftview(const Arg *arg) {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 static const char *firefoxcmd[]  = { "firefox", NULL };
 static const char *emacscmd[]  = { "emacs", NULL };
 static const char *shutdowncmd[]  = { "shutdown", "now", NULL };
